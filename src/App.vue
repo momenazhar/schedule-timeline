465cedbb6eb7126443ad6ac047c2ad5a7923fd29
momenazhar <momenazher57@gmail.com>
2024-02-08 21:00:34 +0300

    styles

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="container">
     <div class="left">
-      <div class="left-list">
-        <div class="filler">SP2024</div>
-        <div class="days">
-          <div class="day" v-for="(day, index) in schedule.days" :key="index">{{ day }}</div>
+      <div class="filler">SPRING 2024</div>
+      <div class="days">
+        <div class="day" v-for="(day, index) in schedule.days" :key="index">{{ day }}</div>
+        <div class="day-lines">
+          <div class="day-line" v-for="index in 5" :key="index" />
         </div>
       </div>
     </div>
     <div class="right">
       <div class="times">
         <div class="time" v-for="(time, index) in schedule.timeline" :key="index">{{ time }}</div>
+        <div class="time-lines">
+          <div class="time-line" v-for="index in schedule.timeline.length" :key="index" />
+        </div>
       </div>
       <div class="grids">
         <div class="lines">
@@ -63,15 +67,30 @@ export default {
 
 <style>
 :root {
-  --horizontal-gap: 1rem;
-  --components-gap: 1rem;
+  --horizontal-gap: 1rem; /* Adjust the gap between the vertical lines and the times (Width of the grid) */
+  --vertical-gap: 4.5rem; /* Adjust the gap between the horizontal lines and the height of each day component (Height of the grid) */
+  --components-gap: 1rem; /* Adjust the gap between each separated element (Semester Box, Times Box, Days Box, Grid Box) */
+  --grid-gap: 2rem; /* Adjust the inline padding of the times and the vertical lines (Preferred to not change) */
+  --top-height: 3rem; /* Adjust the height of the top components (Semester Box and Times Box) */
+  --border-radius: 16px; /* The border radius of all components */
+  --grid-color-primary: rgb(226, 226, 226);
+  --grid-color-secondary: rgb(242, 242, 242);
+  --line-thickness: 2px; /* (Preferred to not change) */
+  --background: rgb(255, 255, 255, 0.4);
+  --stroke: rgb(255, 255, 255, 0.3);
 }
 
 .container {
   display: flex;
   justify-content: center;
+  width: auto;
   flex-direction: row;
+  padding: var(--components-gap);
   gap: var(--components-gap);
+  border-radius: calc(var(--border-radius) + var(--components-gap));
+  backdrop-filter: blur(23px);
+  background-color: var(--background);
+  border: var(--line-thickness) solid var(--stroke);
 }
 
 .right {
@@ -88,54 +107,91 @@ export default {
   align-items: center;
   width: 100%;
   height: 100%;
-  border: 2px solid #e2e2e2;
-  border-radius: 10px;
+  border: var(--line-thickness) solid var(--grid-color-primary);
+  background-color: #fefefe;
+  border-radius: var(--border-radius);
 }
 
 .times {
+  position: relative;
+
   display: flex;
   flex-direction: row;
-  width: 100%;
   justify-content: space-around;
+  align-items: center;
+  color: white;
+  font-weight: 600;
   gap: var(--horizontal-gap);
-  padding-inline: 2rem;
-  height: 3rem;
-  background-color: gray;
-  border-radius: 10px;
+  padding-inline: var(--grid-gap);
+  height: var(--top-height);
+  outline: var(--line-thickness) solid rgb(226, 226, 226, 0.8);
+  background: linear-gradient(180deg, rgba(255, 255, 255, 0.56) 0%, rgba(255, 255, 255, 0.23) 100%);
+  border-radius: var(--border-radius);
   font-variant-numeric: tabular-nums;
 }
 
 .time {
-  height: 3rem;
+  height: var(--top-height);
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.time:nth-child(even) {
+  color: rgb(58, 81, 139, 1);
+  font-size: 12px;
 }
 
 .lines {
   display: flex;
   height: 100%;
   width: 100%;
-  padding-inline: 2rem;
+  padding-inline: var(--grid-gap);
   justify-content: space-around;
   gap: var(--horizontal-gap);
 }
 
-.left {
+.time-lines {
+  position: absolute;
   display: flex;
   flex-direction: row;
+  align-items: baseline;
+  bottom: 0;
+  padding-inline: var(--grid-gap);
+  justify-content: space-around;
+  gap: var(--horizontal-gap);
+  width: 100%;
 }
 
-.left-list {
+.time-line {
+  width: var(--line-thickness);
+  height: 12px;
+  background-color: rgb(226, 226, 226, 0.62);
+}
+
+.time-line:nth-child(even) {
+  height: 8px;
+}
+
+.left {
   display: flex;
   width: 10rem;
   flex-direction: column;
   gap: var(--components-gap);
 }
 
+.days {
+  position: relative;
+  border: var(--line-thickness) solid rgb(226, 226, 226, 0.62);
+  border-radius: var(--border-radius);
+  overflow: hidden;
+}
+
 .day {
-  height: 3rem;
-  background-color: rgb(141, 141, 219);
+  height: var(--vertical-gap);
+  background-color: rgba(231, 234, 255, 0.192);
+  color: white;
+  font-weight: 600;
   position: relative;
   display: flex;
   justify-content: center;
@@ -143,25 +199,40 @@ export default {
 }
 
 .day:first-child {
-  border-radius: 10px 10px 0px 0px;
+  border-radius: var(--border-radius) var(--border-radius) 0px 0px;
 }
 
 .day:last-child {
-  border-radius: 0px 0px 10px 10px;
+  border-radius: 0px 0px var(--border-radius) var(--border-radius);
+}
+
+.day-lines {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  top: 67px;
+  gap: var(--vertical-gap);
+  width: 100%;
+}
+
+.day-line {
+  width: 100%;
+  height: var(--line-thickness);
+  background-color: rgb(226, 226, 226, 0.62);
 }
 
 .filler {
-  height: 3rem;
+  height: var(--top-height);
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 10px;
-  background-color: antiquewhite;
-}
-
-.time:nth-child(even) {
-  color: red;
-  font-size: 12px;
+  border-radius: var(--border-radius);
+  color: white;
+  font-weight: 600;
+  background-color: rgb(29, 33, 89, 0.33);
+  border: var(--line-thickness) solid rgb(29, 33, 89, 0.48);
 }
 
 .grid-container {
@@ -171,12 +242,12 @@ export default {
 
 .grid-horizontal {
   height: 100%;
-  width: 2px;
-  background-color: #e2e2e2;
+  width: var(--line-thickness);
+  background-color: var(--grid-color-primary);
 }
 
 .odd-time {
-  background-color: #f2f2f2;
+  background-color: var(--grid-color-secondary);
 }
 
 .horizontal-lines {
@@ -185,13 +256,13 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 3rem;
+  gap: var(--vertical-gap);
   width: 100%;
 }
 
 .horizontal-line {
   width: 100%;
-  height: 2px;
-  background-color: #e2e2e2;
+  height: var(--line-thickness);
+  background-color: var(--grid-color-primary);
 }
 </style>
